640f38937d48e4eb2102b48d33ca0013b490b15f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-05 04:23:34 -0700

    Task 5 - finally betty is satisfied

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
-
-/*
-* main - Entry point
-* main is a special function.
-* main prints a string to the console and returns 0.
-*
-* Declared a var named Text which gets a pointer to a const string.
-*
-* Used printf to output the value of var Text.
-* printf gets the value of Text and output using %s and new-line.
-* %s: format a string, \n: jump a new-line.
-*
-* Return 0: Don't mind the code just run.
-* return value of main indicates a successful execution.
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ * return  value of main indicates a successful execution.
 */
 int main(void)
 {
