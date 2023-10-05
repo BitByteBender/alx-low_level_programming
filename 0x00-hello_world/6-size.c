e5f819d9419d741d7f887cd558d182858c42e886
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-05 04:20:15 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main(Special Function) - Entry point.
+ * main - Entry point.
  *
  * Return: Always (Success)
  * return  value of main indicates a successful execution.
