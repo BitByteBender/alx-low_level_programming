4b789ba8d8898ab091af84e65045ebc1fd81da7f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-05 05:29:46 -0700

    Task 8 - finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,10 @@ int main(void)
 {
 const char *Text1 = "and that piece of art is useful";
 const char *Text2 = "\" - Dora Korpar, 2016-10-19\n";
-char Quote[2];
+char Quote[59];
+
 sprintf(Quote, "%s%s", Text1, Text2);
 write(1, Quote, 59);
+
 return (1);
 }
