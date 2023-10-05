51916d4ab3ca3f55180ecc2ba042bce0feea9c00
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-05 02:14:31 -0700

    Task 4 - I made a deal with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+* main is a special function.
+* Declare a var named Text that gets a pointer to a constant string.
+* Used puts for single line to output the string var value.
+*
+* Return: Always (Success).
+* return value of main indicates a successful execution.
+*/
 int main(void)
 {
-	const char *Text = "\"Programming is like building a multilingual puzzle";
-	puts(Text);
-	return (0);
+const char *Text = "\"Programming is like building a multilingual puzzle";
+puts(Text);
+return (0);
 }
