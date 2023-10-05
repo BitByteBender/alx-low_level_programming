c7d4e20ae123dd00d5e7c945e30cec6bb1036b69
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-05 05:22:24 -0700

    Task 8 - Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,17 @@
+#include <stdio.h>
 #include <unistd.h>
-/*
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-const char *Text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, Text, 59);
+const char *Text1 = "and that piece of art is useful";
+const char *Text2 = "\" - Dora Korpar, 2016-10-19\n";
+char Quote[2];
+sprintf(Quote, "%s%s", Text1, Text2);
+write(1, Quote, 59);
 return (1);
 }
