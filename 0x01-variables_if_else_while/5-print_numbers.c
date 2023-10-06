d519f80a7d577cc179b412cce0d9d6b38f2383ed
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-06 00:37:21 -0700

    Task 5 - Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+short i = 0;
+while (i <= 9)
+{
+if (i <= 9)
+{
+printf("%d", i);
+i++;
+}
+else
+{
+break;
+}
+}
+putchar('\n');
+return (0);
+}
