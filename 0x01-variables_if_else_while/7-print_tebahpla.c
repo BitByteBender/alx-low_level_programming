e1df8af6fe48e82928a09168da8759aa3cb3cb29
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-06 00:51:09 -0700

    Task 7 - Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+short i = 122;
+while (i >= 97)
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
