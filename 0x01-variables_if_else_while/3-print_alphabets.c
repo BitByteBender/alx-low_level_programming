f5b114825a1bb695246d94383d1abd301217ca9c
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-06 00:19:54 -0700

    Task 3 - alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned short i = 97;
+while (i <= 122)
+{
+putchar(i);
+i++;
+}
+
+i = 65;
+while (i <= 90)
+{
+putchar(i);
+i++;
+}
+
+putchar('\n');
+
+return (0);
+}
