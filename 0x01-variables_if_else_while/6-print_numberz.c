a2812bb0215817886b9ad4d4f4c46f9a4ff39554
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-06 00:46:26 -0700

    Task 6 - Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+short i = 48;
+while (i < 58)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
