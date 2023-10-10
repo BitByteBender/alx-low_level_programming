a3d1887131f3ca80a6fb2a95db492555b2db5c0c
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-10 04:32:26 -0700

    Added main.h header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+
+void PrintCharacters();
+
+void print_alphabet();
+
+void print_alphabet_x10();
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+#endif
