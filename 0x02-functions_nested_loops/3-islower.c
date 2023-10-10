57254911d52ef1e701890f93dca3f1996ef8a5cb
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-10 03:04:18 -0700

    Task 3 - islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - takes no parameters
+ *
+ * loops from 97 to 122 which is a to z(ASCII)
+ *
+ * new line at the end
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 127 ? 1 : 0);
+}
