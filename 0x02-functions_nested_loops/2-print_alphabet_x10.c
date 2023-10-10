f976df637b7a4bdc1e15ccced37242cfe2cbd7bb
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-10 02:18:42 -0700

    Task 2 - 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - takes no parameters
+ *
+ * First loop: loops 10 times
+ * Second loop: loops from 97 to 122 which is a to z(ASCII)
+ *
+ * new line at the end
+ */
+void print_alphabet_x10(void)
+{
+
+unsigned short i;
+for (i = 1; i <= 10; i++)
+{
+
+unsigned short j;
+for (j = 97; j <= 122; j++)
+{
+	_putchar(j);
+}
+	_putchar('\n');
+}
+}
