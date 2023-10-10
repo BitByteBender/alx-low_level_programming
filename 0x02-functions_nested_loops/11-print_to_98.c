55fdd3ae22aa4e5dedf75ad5b37654ffd91fcd69
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-10 13:39:39 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,11 +5,14 @@
  *
  * @n: integer param
  *
- * Loops from n to 98
+ * Loops from n to 98 if n < 98
+ * Loops backwards if n > 98
  */
 void print_to_98(int n)
 {
 int i;
+if (n <= 98)
+{
 for (i = n; i <= 98; i++)
 {
 	if (i != 98)
@@ -20,5 +23,20 @@ for (i = n; i <= 98; i++)
 	{
 		printf("%d\n", i);
 	}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+	if (i != 98)
+	{
+		printf("%d, ", i);
+	}
+	else
+	{
+		printf("%d\n", i);
+	}
+}
 }
 }
