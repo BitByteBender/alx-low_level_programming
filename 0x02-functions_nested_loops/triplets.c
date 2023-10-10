0f00ab5b0c503bd8a006c5c5d2fd0bfd8f3497b2
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-10 10:44:36 -0700

    Added triplets

---
+++ 0x02-functions_nested_loops/triplets.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _triplets - takes 2 integers and 1 char as params
+ *
+ * @n1: integer param
+ * @n2: interger param
+ * @c: char param
+ *
+ * _putchar: to print values
+ */
+void _triplets(unsigned short n1, unsigned short n2, char c)
+{
+	_putchar(n1);
+	_putchar(n2);
+	_putchar(c);
+}
