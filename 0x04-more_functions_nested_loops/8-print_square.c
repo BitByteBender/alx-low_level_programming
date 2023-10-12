7e66975f49f55a948e44f9fc2ee1b664287d3840
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-12 06:36:50 -0700

    Task 8 - You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - takes an integer parameter
+ * @size: is going to be fed from 8-main.c
+ *
+ * Used if statements to check the value of @size
+ * Var i & j are a loop iterators
+ * for loop from 1 till @size
+ * _putchar used for printing
+ */
+void print_square(int size)
+{
+  unsigned short i, j;
+
+  if (size == 0)
+  {
+    _putchar('\n');
+  }
+  else
+  {
+    for (i = 1; i <= size; i++)
+    {
+      for (j = 1; j <= size; j++)
+      {
+	_putchar('#');
+      }
+      _putchar('\n');
+    }
+  }
+}
