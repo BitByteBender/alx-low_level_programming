1e21792e395a880a9323b3f1354d3af5fe2d973f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-12 02:27:24 -0700

    Task 2 - Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - takes two integer params
+ * @a: is an integer param
+ * @b: is an integer param
+ *
+ * Return: Multiplication result of @a & @b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
