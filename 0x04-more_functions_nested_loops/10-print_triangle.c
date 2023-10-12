9a18fbbbf066233af4e1188eaa882c374427826b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-12 09:47:53 -0700

    Task 10 - Everything works fine

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -22,16 +22,16 @@ void print_triangle(int size)
 	{
 	for (i = 1; i <= size; i++)
 	{
-		for (j = i + 1; j <= size; j++)
-		{
-		_putchar(' ');
-		}
+	for (j = i + 1; j <= size; j++)
+	{
+	_putchar(' ');
+	}
 
-		for (k = 0; k < i; k++)
-		{
-		_putchar('#');
-		}
-		_putchar('\n');
+	for (k = 0; k < i; k++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
 	}
 	}
 }
