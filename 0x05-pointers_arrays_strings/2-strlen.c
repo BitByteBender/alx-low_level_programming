74bb4a7cdcd248ea5fe65ee2c15675891ac5dc38
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 02:17:22 -0700

    Task 2 - This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - Takes a char pointer as a param
+ * @s: a pointer to the 1st char of a string
+ * strlen(): Calculates the length of a string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
