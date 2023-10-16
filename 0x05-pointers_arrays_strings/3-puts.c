6aff67660a629f825a0e9c616b51e309e72524d3
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 03:29:03 -0700

    Task 3 - I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts - Takes a char pointer as a param
+ * @str: a pointer to the 1st char of a string
+ * strlen(): Calculates the length of a string
+ * loop: goes from 0 to array of chars - 1
+ * [i]: i is an array's index
+ * string is an array of chars
+ * _putchar for printing
+ */
+void _puts(char *str)
+{
+	unsigned short i;
+
+	for (i = 0; i <= (strlen(str) - 1); i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
