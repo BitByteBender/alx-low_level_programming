59a69509194bfa90b7920aa474bf17c6f04a93af
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 07:22:59 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,7 +4,7 @@
  * puts_half - Takes a char pointer as a param
  * @str: a pointer to the 1st char of a string
  * strlen(): Calculates the length of a string
- * loop: starts from half of the array to (array of chars - 1)
+ * loop: starts from half of the array to(array of chars - 1)
  * [i]: i is an array's index
  * string is an array of chars
  * _putchar for printing
@@ -13,18 +13,18 @@ void puts_half(char *str)
 {
 	unsigned short i;
 
-	if (str == NULL || str[0] == '\0')
-	{
-		_putchar(' ');
-		return;
-	}
-	else
-	{
 	for (i = ((strlen(str) + 1) / 2); i <= (strlen(str) - 1); i++)
 	{
-
+		if (str == NULL || str[0] == '\0')
+		{
+			_putchar(' ');
+			break;
+		}
+		else
+		{
 		_putchar(str[i]);
+		}
 	}
-	}
+
 	_putchar('\n');
 }
