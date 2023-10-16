2cc69e76ed9bad6ffbac9f8cb2acca0c2c439e75
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 04:30:33 -0700

    Task 4 - I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include "main.h"
+/**
+ * print_rev - Takes a char pointer as a param
+ * @s: a pointer to the 1st char of a string
+ * strlen(): Calculates the length of a string
+ * loop backwards: goes from last array index to index 0
+ * string is an array of chars
+ * _putchar for printing
+ */
+void print_rev(char *s)
+{
+	short i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		if (s == NULL)
+		{
+			_putchar(' ');
+			break;
+		}
+		else
+		{
+
+		_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
