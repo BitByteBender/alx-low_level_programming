6058f320fa422f409ecd9357d74d052b72f16f40
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 01:56:30 -0700

    Task 1 - Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int	- takes two integers as a pointer params
+ * @a: is an int pointer that holds addresses
+ * @b: is an int pointer that holds address of another data type
+ * temp: is a temporary container for mutating @a & @b
+ */
+void swap_int(int *a, int *b)
+{
+short temp = 0;
+temp = *a;
+*a = *b;
+*b = temp;
+}
