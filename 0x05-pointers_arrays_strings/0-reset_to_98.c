1eee5d5b0fddd67448113f9a5c2f1df913459e2b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 01:42:37 -0700

    Task 0 - Betty! here we go again

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * reset_to_98 - takes an integer as a pointer param
- * @*n: is an int pointer that holds only addresses
+ * @n: is an int pointer that holds only addresses
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }
