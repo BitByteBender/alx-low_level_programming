66fa38ed95bc43e21308b0dec4839eb19f3a57d7
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-16 08:39:01 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - Copies a string from src to dest
+ * @dest: a pointer to the dest of a string array
+ * @src: a pointer to src array(string) of characters
+ * loop: starts from 1st index of the array to last index
+ * strlen(): Calculates the length of a string
+ * Return: pointer to dest(full string)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	unsigned short i;
+
+	for (i = 0; i < strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
