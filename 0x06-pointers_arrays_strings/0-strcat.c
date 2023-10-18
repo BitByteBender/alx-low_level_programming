1e8e525a92d574aceb8504cb8443f2eb1eba90ef
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-18 03:54:15 -0700

    Task 0 - strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - takes 2 char pointers as params
+ * @dest: a pointer to the destination of a string array
+ * @src: a pointer to the src array(string) of chars
+ * loop: starts from the index 0 to full length of dest
+ * strlen(): Calculates the length of a string
+ * Condition to check if 0 byte or not
+ * Return: pointer to dest(full string)
+ */
+char *_strcat(char *dest, char *src)
+{
+	unsigned short i, dest_length = strlen(dest);
+
+	for (i = 0; i <= dest_length; i++)
+	{
+		if (src[i] == '\0')
+		{
+			break;
+		}
+		else
+		{
+		dest[dest_length + i] = src[i];
+		}
+	}
+
+	dest[dest_length + i] = '\0';
+	return (dest);
+}
