add1557c6957628c8d768f0f2762bc0ad9b14d52
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-23 03:51:49 -0700

    Task 2 - strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - search for the 1st identical char in a string
+ * @s: string to be fetched
+ * @c: char used to locate a char in a @s string
+ *
+ * Return: 
+ * if @c is found return the string part from char @c
+ * else return NULL
+ */
+char *_strchr(char *s, char c)
+{
+	if (c == '\0')
+	{
+		return s;
+	}
+	else
+	{
+		for (; *s != '\0'; s++)
+		{
+			if (*s == c)
+			{
+			return s;
+			}
+		}
+	}
+
+	return (NULL);
+}
