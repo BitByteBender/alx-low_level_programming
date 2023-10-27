7e00a912a5900fd4e20cc696ba42e8091b706ac1
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-27 08:23:12 -0700

    Task 0 - It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,13 @@
  * main - Entry point takes two args
  * @argc: intger argument count
  * @argv: argument value (array of chars pointers)
+ * argument count intentionally not used
  * prints argument value
- * Return: argc (argument count)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char **argv)
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
