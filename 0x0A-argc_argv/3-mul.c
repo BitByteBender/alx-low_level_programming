1bc3bcc572d67c1b1b039ca8d920586f593abcee
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-27 06:40:00 -0700

    Task 3 - Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point takes two args
+ * @argc: intger argument count
+ * @argv: argument value (array of chars pointers)
+ * If arguments less than 2 print Error
+ * Else Iterates from index 1 then calculate till max count
+ * Stored calculation inside Result
+ * used atoi() to convert argument at current index to int
+ *
+ * Return:
+ * 1 if argument count is less than 2
+ * argc (argument count)
+ */
+int main(int argc, char *argv[])
+{
+	unsigned short i;
+	long int Result = 1;
+
+	if (argc < 2)
+	{
+	puts("Error");
+	return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			Result *= atoi(argv[i]);
+		}
+
+	}
+	printf("%ld\n", Result);
+	return (argc);
+}
