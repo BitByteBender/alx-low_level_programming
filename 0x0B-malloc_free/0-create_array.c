012a95db58009aae5de7a76dc29ade8928242f2f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-30 05:42:06 -0700

    Task 0 - Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * create_array - a char pointer function that fills an array with @c
+ * @size: an integer param
+ * @c: a char param
+ * size used to check if 0 return NULL
+ * Else
+ * declared a string and gave it a size using malloc()
+ * then iterate over it till the @size
+ * then fill the str var (array of chars) with @c at every index
+ * starting from 0
+ * Return:
+ * if size is 0 return NULL otherwise, return @str
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		unsigned short i;
+
+		str = malloc(sizeof(*str) * size);
+
+		for (i = 0; i < size; i++)
+		{
+			str[i] = c;
+		}
+	}
+
+	str[size] = '\0';
+	return (str);
+}
