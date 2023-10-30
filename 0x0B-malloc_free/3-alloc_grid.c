51b767bd6822209871744bbfa796f7223786151e
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-30 13:14:11 -0700

    Task 3 - If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,7 +36,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i <= (height - 1); i++)
 	{
-		grid[i] = malloc(sizeof(*grid) * width);
+		grid[i] = malloc(sizeof(grid) * width);
 
 		if (grid[i] == NULL)
 		{
