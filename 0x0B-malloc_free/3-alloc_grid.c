7cea091f8e6b6d43fc1aae300575af673d6e6dee
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-30 13:12:28 -0700

    Task 3 - If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - Allocates a two dimensional grid
+ * of integers and init them to 0
+ * @width: number of grid columns
+ * @height: number of grid rows
+ * Checks if width or height is 0 or negative
+ * first malloc:
+ * allocate memory to rows
+ * second malloc:
+ * allocate memory to columns
+ * Loop: loops through each row and allocate memory
+ * if inside the loop checks if allocation for rows fails
+ * Return:
+ * if allocation(success) return a ptr to 2D grid
+ * if allocation(fails or height/width is 0 or negative)
+ * then return NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	unsigned short i = 0, j = 0;
+	int **grid;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = malloc(sizeof(*grid) * height);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= (height - 1); i++)
+	{
+		grid[i] = malloc(sizeof(*grid) * width);
+
+		if (grid[i] == NULL)
+		{
+		return (NULL);
+		}
+		for (j = 0; j <= (width - 1); j++)
+		{
+		grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,6 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 
 char *str_concat(char *, char *);
+
+int **alloc_grid(int , int);
 #endif /* MAIN_H */
