f71a6383c04b393ee6d274003aa40d23ad39e9c1
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-10-30 10:21:13 -0700

    Task 2 - He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - Allocate a memory & return
+ * a pointer to a copied string var s1_s2
+ * @s1: first string to be duplicated
+ * @s2: second string to be copied and concatenated
+ * var s1_s2: string container used for copying and concatenation
+ * used malloc() for memory allocation
+ * Checks:
+ * if string @s1 and @s2 is NULL
+ * if var s1_s2 got memory allocation
+ *
+ * for loop for copying @s1 to var s1_s2 by index
+ * after a successful memory allocation
+ * condition within loop does a check to copy and concatenate succesfully
+ * Return: Pointer to the newly allocated memory
+ * var s1_s2(full string)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s1_s2;
+
+	if (s1 == NULL && s1 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		unsigned short i, Length = (strlen(s1) + strlen(s2));
+
+		s1_s2 = malloc(Length + 1);
+
+		if (s1_s2 == NULL)
+		{
+			return (NULL);
+		}
+
+	for (i = 0; i <= Length; i++)
+	{
+		if (i >= strlen(s1))
+		{
+			s1_s2[i] = s2[i - strlen(s1)];
+		}
+		else
+		{
+			s1_s2[i] = s1[i];
+		}
+	}
+	s1_s2[Length] = '\0';
+	}
+
+	return (s1_s2);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,4 +6,6 @@ int _putchar(char);
 char *create_array(unsigned int, char);
 
 char *_strdup(char *);
+
+char *str_concat(char *, char *);
 #endif /* MAIN_H */
