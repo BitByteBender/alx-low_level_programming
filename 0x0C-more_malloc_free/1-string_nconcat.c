52ddd3c7e27fd643bf23df6fb7aac1ed02c67f21
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-02 12:37:25 -0700

    Task 1 - fix number 1 billion

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s1_s2;
-	unsigned short i = 0, j = 0, s1Length = 0, s2Length = 0, s1_s2Length = 0;
+	unsigned short i = 0, s1Length = 0, s2Length = 0, s1_s2Length = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,25 +24,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	s1Length = strlen(s1);
 	s2Length = strlen(s2);
-	s1_s2Length = s1Length + n;
 
 	if (n >= s2Length)
-		s1_s2Length += s2Length;
+		s1_s2Length = s1Length + s2Length;
 	else
-		s1_s2Length += n;
+		s1_s2Length = s1Length + n;
 
 	s1_s2 = (char *)malloc(sizeof(char) * s1_s2Length + 1);
 
-	if (s1_s2 == NULL)
-		return (NULL);
-
+	if (s1_s2 != NULL)
+	{
 	for (i = 0; i <= (s1Length - 1); i++)
 		s1_s2[i] = s1[i];
 
-	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
-		s1_s2[i] = s2[j];
+	for (i = 0; i <= (n - 1) && i < s2Length; i++)
+		s1_s2[i + s1Length] = s2[i];
 
-	s1_s2[i] = '\0';
+	s1_s2[i + s1Length] = '\0';
+	}
 
 	return (s1_s2);
 }
