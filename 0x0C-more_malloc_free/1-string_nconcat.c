b57ddd1151f354eb20f77e1313de7804ef3ae6ab
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-02 06:25:48 -0700

    Task 1 - Hot Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,7 +6,7 @@
  * string_nconcat - Allocates memory if string @s1 & @s2 not NULL
  * @s1: string to be copied
  * @s2: string to be concatenated
- * @n: length/truncater for @s1 & @s2
+ * @n: max number/length/truncater for @s1 & @s2
  * s1_s2: newly allocated string
  * checks if @s1 and @s2 are not NULL
  * Return:
@@ -15,29 +15,35 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s1_s2;
-	unsigned short i = 0, s2Length = strlen(s2);
-	unsigned short Length = 0;
+	unsigned short i = 0, s1Length = strlen(s1), s2Length = strlen(s2);
+	unsigned short s1_s2Length = 0;
 
-	(s1 == NULL ? s1 = "" : s2 == NULL ? s2 = "" : "");
+	if (s1 == NULL)
+		s1 = "";
 
-	(n >= s2Length ? (Length = (s2Length * 2 + 1)) : (Length = (n * 2) + 1));
+	if (s2 == NULL)
+		s2 = "";
 
-	s1_s2 = (char *)malloc(Length);
+	if (n >= s2Length)
+		s1_s2Length = (s1Length + s2Length);
+	else
+		s1_s2Length = (s1Length + n);
+
+	s1_s2 = (char *)malloc(s1_s2Length + 1);
 
 	if (s1_s2 != NULL)
 	{
 		for (i = 0; i <= (n - 1); i++)
 		{
-		s1_s2[i] = s1[i];
+			s1_s2[i] = s1[i];
 		}
 
 		for (i = 0; i <= (n - 1); i++)
 		{
-		s1_s2[i + (n - 1)] = s2[i];
+			s1_s2[i + (n - 1)] = s2[i];
 		}
+	s1_s2[s1_s2Length] = '\0';
 	}
-
-	s1_s2[Length] = '\0';
 
 	return (s1_s2);
 }
