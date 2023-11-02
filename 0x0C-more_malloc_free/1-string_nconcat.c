cee9c810d0c59ea5d37b27b66491972501bfabf3
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-02 06:30:42 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1_s2 != NULL)
 	{
-		for (i = 0; i <= (n - 1); i++)
+		for (i = 0; i <= (s1Length - 1); i++)
 		{
 			s1_s2[i] = s1[i];
 		}
