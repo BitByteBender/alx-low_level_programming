80b2d5536005928d3649f6acf6891c53b1d0e2be
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-04 02:05:06 -0700

    Task 5 - frees allocated memory for dog struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - a function that frees allocated memory
+ * @d: a pointer to dog struct
+ * frees if allocated memory for dog struct and its members
+ * checks if @d is not NULL
+ * frees memory for dog's name, dog's owner and then frees struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	if (d->name != NULL)
+		free(d->name);
+
+	if (d->owner != NULL)
+		free(d->owner);
+
+	free(d);
+	}
+	else
+	{
+		free(d);
+	}
+}
