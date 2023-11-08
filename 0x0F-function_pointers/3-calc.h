625760042f1fc24ce7f5ffdf3caf9959627070fd
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-08 12:50:30 -0800

    Task 3 - fixed potential error: requirement-fail

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
  * struct op - Struct op
  *
@@ -9,23 +8,23 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 typedef int (*Operation_func)(int, int);
 
 Operation_func get_op_func(char *op);
 
-int op_add(int, int);
+int op_add(int a, int b);
 
-int op_sub(int, int);
+int op_sub(int a, int b);
 
-int op_mul(int, int);
+int op_mul(int a, int b);
 
-int op_div(int, int);
+int op_div(int a, int b);
 
-int op_mod(int, int);
+int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
 #endif /* CALC_H */
