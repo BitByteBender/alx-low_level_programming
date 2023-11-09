2b996a6e4acc526429ca48cf9a88072a33d593d8
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-09 03:43:40 -0800

    Task 2 - print n strings arguments using variadic function

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints @n strings args using a separator in between
+ * @n: number of args that will get fed
+ * @separator: a string(char pointer) to separate @n args
+ * var Holder: string to be printed
+ * check if @separator is NULL or not
+ * if not NULL prints @n args followed by @separator
+ * while i(counter) is less than (@n - 1) and once i < n print a new line
+ * Checks if string is NULL or not
+ * if NULL prints nil otherwise print the whole string
+ * otherwise print @n args without @separator and end the full print using
+ * new line
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	char *Holder;
+
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+	Holder = va_arg(args, char *);
+
+	if (separator != NULL)
+		if (Holder != NULL)
+			(i < (n - 1) ? printf("%s%s", Holder, separator) : printf("%s\n", Holder));
+		else
+			(i < (n - 1) ? printf("nil%s", separator) : puts("nil"));
+	else
+		if (Holder != NULL)
+			(i < (n - 1) ? printf("%s", Holder) : printf("%s\n", Holder));
+		else
+			(i < (n - 1) ? printf("nil") : puts("nil"));
+	}
+
+	va_end(args);
+}
