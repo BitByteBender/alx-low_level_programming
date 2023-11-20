59745c579e53ab6ff9204488aaaa634aef1cdc3f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-20 10:56:38 -0800

    Task 9 - fixed a loop condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	thisNode = *head;
 	if (thisNode != NULL)
 	{
-	while (i <= (idx - 1))
+	while (i < (idx - 1))
 	{
 	thisNode = thisNode->next;
 	i++;
