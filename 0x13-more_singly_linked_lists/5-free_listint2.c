dd72d16bb04795433211cd64883be65f469c2987
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-20 03:38:04 -0800

    Task 5 - Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_listint2 - frees the allocated memory for listint_t
+ * @head: pointer to the head of listint_t
+ * function sets the head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *thisNode = *head;
+	listint_t *nextNode;
+
+	while (thisNode != NULL)
+	{
+	nextNode = thisNode->next;
+	free(thisNode);
+	thisNode = nextNode;
+	}
+
+	*head = NULL;
+}
