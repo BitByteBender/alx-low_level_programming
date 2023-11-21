34bcc17490dc7870dfe3a924d6931bde5cf8a938
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-21 15:32:46 -0800

    Task 5 - Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,9 @@ void free_listint2(listint_t **head)
 	listint_t *thisNode = *head;
 	listint_t *nextNode;
 
+	if (head == NULL)
+		return;
+
 	while (thisNode != NULL)
 	{
 	nextNode = thisNode->next;
