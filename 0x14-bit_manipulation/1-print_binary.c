d1d89e09b04b79ed6880f848c775dd50c88d6de8
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-23 10:10:45 -0800

    Task 1 - decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,19 @@
 #include "main.h"
-#include <string.h>
+/**
+ * _pow - performs power calculation
+ * @base: base num to be powered
+ * @expo: exponent value
+ * Return: overall result
+ */
+unsigned int _pow(unsigned int base, unsigned int expo)
+{
+	unsigned short i, exponent = 1;
+
+	for (i = 0; i < expo; i++)
+		exponent *= base;
+
+	return (exponent);
+}
 /**
  * print_binary - prints the conversion from decimal to binary
  * @n: decimal value to get converted
