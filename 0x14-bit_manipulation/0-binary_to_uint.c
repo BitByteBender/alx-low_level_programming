e794b8a653c3dc26f6c32788b09a706ce5901816
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-23 10:12:05 -0800

    Task 0 - binary string to decimal uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _pow - performs power calculation
+ * @base: base num to be powered
+ * @expo: exponent value
+ * Return: overall result
+ */
+unsigned int _pow(unsigned int base, unsigned int expo)
+{
+	unsigned short i, exponent = 1;
+
+	for (i = 0; i < expo; i++)
+		exponent *= base;
+
+	return (exponent);
+}
+/**
  * binary_to_uint - converts binary(string) to base 10(integer)
  * @b: binary string
  * Return: converted binary(string) to uint decimal(base 10)
