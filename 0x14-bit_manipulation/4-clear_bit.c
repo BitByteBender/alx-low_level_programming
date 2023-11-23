5726fa6be1216517ac462427efd51f7147575c53
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-23 13:08:48 -0800

    Task 4 - Clears the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * clear_bit - clears the value of a bit at a given index to 0
+ * @n: pointer to ul number
+ * @index: the index of the bit to clear, starting from 0
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	const unsigned long ulBits = sizeof(*n) * 8;
+	unsigned long Condition = (1 << index);
+
+	if (n == NULL || index >= ulBits)
+	{
+		return (-1);
+	}
+	else if ((*n & Condition) != 0)
+	{
+		*n &= ~Condition;
+		return (1);
+	}
+
+	return (0);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -12,4 +12,6 @@ void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 
 int set_bit(unsigned long int *, unsigned int); 
+
+int clear_bit(unsigned long int *, unsigned int);
 #endif /* MAIN_H */
