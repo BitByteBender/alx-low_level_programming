5fc148fe6128f622b4e78dac7a268b671f654096
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2023-11-23 13:19:58 -0800

    Task 4 - fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,7 +15,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	{
 		return (-1);
 	}
-	else if ((*n & Condition) != 0)
+	else if ((*n & Condition) != 0 && Condition != 0)
 	{
 		*n &= ~Condition;
 		return (1);
