f9043861b38fdff6c8646763f889150e870ddcc4
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-08 16:04:22 -0800

    Task 5 - retrieve node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns the nth node of dlistint_t
+ * @head: pointer the dlistint_t head
+ * @index: index of the node to get starts from 0
+ * Return: pointer to the nth node
+ * or
+ * NULL if it doesn't exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned short i = 0;
+
+	while (head != NULL && i <= (index - 1))
+	{
+	head = head->next;
+	i++;
+	}
+
+	if (head && i == index)
+		return (head);
+	else
+		return (NULL);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,4 +23,5 @@ size_t dlistint_len(const dlistint_t *);
 dlistint_t *add_dnodeint(dlistint_t **, const int);
 dlistint_t *add_dnodeint_end(dlistint_t **, const int);
 void free_dlistint(dlistint_t *);
+dlistint_t *get_dnodeint_at_index(dlistint_t *, unsigned int);
 #endif /* LIST_H */
