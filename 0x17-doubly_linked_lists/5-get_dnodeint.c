f7808e93925d92b9b711ea0b672d43007f1c4231
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-09 09:19:16 -0800

    Task 5 - Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned short i = 0;
 
-	while (head != NULL && i <= (index - 1))
+	while (i < (index - 1) && head != NULL)
 	{
 	head = head->next;
 	i++;
