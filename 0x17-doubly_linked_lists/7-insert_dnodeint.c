09895ef556e4683772314011fb59c187a2454381
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-01-09 09:03:54 -0800

    Task 7 - insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+dlistint_t *dnodeGenerator(short, unsigned int);
 /**
  * insert_dnodeint_at_index - inserts newNode at an idx in dlistint_t
  * @h: pointer to head pointer of dlistint_t
@@ -9,43 +10,63 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newNode, *thisNode;
+	dlistint_t *newNode, *thisNode = *h;
 	unsigned short i = 0;
 
 	if (h == NULL)
 		return (NULL);
 
-	newNode = (dlistint_t *)malloc(sizeof(dlistint_t *) * (idx + n));
+	newNode = (dlistint_t *)dnodeGenerator((int)n, idx);
 	if (newNode == NULL)
 		return (NULL);
-
-	newNode->n = n;
 
 	if (idx == 0)
 	{
 	newNode->next = *h;
 	newNode->prev = NULL;
-	return (*h = newNode);
-	}
-
-	thisNode = *h;
-	if (thisNode != NULL)
-	{
-	while (i < (idx - 1))
-	{
-	thisNode = thisNode->next;
-	i++;
-	}
+	if (*h != NULL)
+		(*h)->prev = newNode;
+	*h = newNode;
 	}
 	else
 	{
-	free(newNode);
-	return (NULL);
-	}
-
+		if (thisNode != NULL)
+		{
+		while (i < (idx - 1))
+		{
+		thisNode = thisNode->next;
+		i++;
+		}
+		}
+		else
+		{
+		free(newNode);
+		return (NULL);
+		}
 	newNode->next = thisNode->next;
 	newNode->prev = thisNode;
+	if (thisNode->next != NULL)
+		thisNode->next->prev = newNode;
 	thisNode->next = newNode;
+	}
+	return (newNode);
+}
+/**
+ * dnodeGenerator - generates a new dlistint_t node
+ * @n: value to be inserted in the newNode
+ * @idx: index of the list
+ * Return: the address of the newNode or NULL if it failed
+ */
+dlistint_t *dnodeGenerator(short n, unsigned int idx)
+{
+	dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t *) * (idx + n));
+
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = NULL;
 
 	return (newNode);
 }
